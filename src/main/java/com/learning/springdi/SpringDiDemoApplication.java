fde0a9faaa8b36a93e64f9304d5cd53751028610
Shiva Krishnan <shivainfotech12@gmail.com>
2018-12-28 12:54:44 +0530

    Component Scan Sample Added

--- src/main/java/com/learning/springdi/SpringDiDemoApplication.java
+++ src/main/java/com/learning/springdi/SpringDiDemoApplication.java
@@ -3,13 +3,16 @@ package com.learning.springdi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
+import com.learning.outerworld.OuterWorldService;
 import com.learning.springdi.controllers.ConstructorInjectedController;
 import com.learning.springdi.controllers.MyController;
 import com.learning.springdi.controllers.PropertyInjectedController;
 import com.learning.springdi.controllers.SetterInjectedController;
 
 @SpringBootApplication
+@ComponentScan(basePackages= {"com.learning.outerworld", "com.learning.springdi"})
 public class SpringDiDemoApplication {
 
 	public static void main(String[] args) {
@@ -26,6 +29,10 @@ public class SpringDiDemoApplication {
 		
 		SetterInjectedController sic = (SetterInjectedController)ctx.getBean(SetterInjectedController.class);
 		System.out.println(sic.sayHello());
+		
+		
+		OuterWorldService ows = ctx.getBean(OuterWorldService.class);
+		System.out.println(ows.getMessage());
 	}
 
 }

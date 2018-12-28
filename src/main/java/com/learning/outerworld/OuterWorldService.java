9746f6269a0026d042fa2eee3e4449867a50b035
Shiva Krishnan <shivainfotech12@gmail.com>
2018-12-28 12:55:00 +0530

    Component Scan Sample Added 1

---
+++ src/main/java/com/learning/outerworld/OuterWorldService.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.learning.outerworld;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author shivaak on 28-Dec-2018
+ *
+ */
+@Service
+public class OuterWorldService {
+
+	public String getMessage() {
+		return "Hey! This is a message from Outter World";
+	}
+}
